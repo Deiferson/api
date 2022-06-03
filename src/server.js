9db350c7b5b095b5a11d97c9ce714d53e0fb6665
Deiferson Moura <deifersondasilvamoura@gmail.com>
2022-06-03 14:26:28 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ const app = express();
 
 const port = 5000;
 
+app.get('/', (req, res) => {
+     console.log('ok')
+     res.send('ok')
+});
+
 app.listen(port, () =>{
     console.log(`Executando ... localhost:${port}`)
 })
